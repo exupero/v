8296e3c43409fbccdfcacacb404411264570cc8f
Eric Shull <shull@atomicobject.com>
2015-01-02 06:57:11 -0500

    Support piping JSON data into v executable.

--- src/v.js
+++ src/v.js
@@ -1,2 +1,2 @@
 var v=require('./v.core'),ss=document.querySelectorAll('script[type="text/v"]'),i;
-for(i=0;i<ss.length;i++){var s=ss[i],src=s.childNodes[0].data,el=document.createElement('div');s.parentNode.replaceChild(el,s);v.call(el,src)}
+for(i=0;i<ss.length;i++){var s=ss[i],src=s.childNodes[0].data,el=document.createElement('div');s.parentNode.replaceChild(el,s);v.call(el,src,null,{data:window.vdata})}
--- v.go
+++ v.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"flag"
+	"io/ioutil"
 	"text/template"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/toqueteos/webbrowser"
 )
 
 var port = flag.Int("p", 8000, "Port")
-var indexTemplate = `<!DOCTYPE html><body><script type="text/v">{{.src}}</script><script type="text/javascript">{{.v}}</script></body>`
+var indexTemplate = `<!DOCTYPE html><body>
+<script type="text/v">{{.src}}</script>
+<script type="text/javascript">window.vdata = {{.data}}</script>
+<script type="text/javascript">{{.v}}</script>
+</body>`
 
 func main() {
 	flag.Parse()
@@ -19,12 +25,22 @@ func main() {
 	url := fmt.Sprintf("0.0.0.0:%d", *port)
 	src := flag.Arg(0)
 	indexPage := template.Must(template.New("index").Parse(indexTemplate))
+	data := ""
+
+	fi, err := os.Stdin.Stat()
+	if err != nil { log.Fatal(err) }
+
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		b, err := ioutil.ReadAll(os.Stdin)
+		if err != nil { log.Fatal(err) }
+		data = string(b)
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Add("Content-Type", "text/html")
 		v, err := Asset("v.min.js")
 		if err != nil { http.Error(w, err.Error(), http.StatusInternalServerError) }
-		data := map[string]string{"src": src, "v": string(v)}
+		data := map[string]string{"src": src, "v": string(v), "data": data}
 		if err := indexPage.Execute(w, data); err != nil { http.Error(w, err.Error(), http.StatusInternalServerError) }
 	})
 
